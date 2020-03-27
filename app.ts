9bb5c1851b381f2d32a79b246ec70463960cddb9
Robert McBryde <robert.mcbryde@ros.gov.uk>
2020-03-27 16:11:03 +0000

    playing with Typescrit basic types and inference

--- app.ts
+++ app.ts
@@ -1,1 +1,11 @@
-console.log('Your code goes here...');
+const person = {
+    name: 'Rob',
+    age: 36,
+    hobbies: ['Sports', 'Cooking']
+};
+
+console.log(person.name);
+
+for (const hobby of person.hobbies) {
+    console.log(hobby.toUpperCase());
+};
